6497de569f0127abad7b980104bed0ad3a54b2c3
TwinkaljitSingh <twinkaljit.singh@flemingcollege.ca>
2024-01-15 13:43:42 -0500

    modified both files in the clone repository
    -deleted a print statement from script1.py
    -added print statement to print2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # Print lines from Zen of Python
 print("Unless explicitly silenced.")
-print("In the face of ambiguity, refuse the temptation to guess.")
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # Print lines from Zen of Python
 print("There should be one-- and preferably only one --obvious way to do it.")
 print("Although that way may not be obvious at first unless you're Dutch.")
+print("In the face of ambiguity, refuse the temptation to guess.")
